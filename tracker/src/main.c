8ea50a8b6e8c81d9fc1aeb87b4982a448c3af945
Michael Moffitt <mikejmoffitt@gmail.com>
2015-12-21 23:51:23 -0500

    paramerized test values for tracker

--- tracker/src/main.c
+++ tracker/src/main.c
@@ -7,7 +7,20 @@
 
 int main(int argc, char **argv)
 {
+	if (argc > 1)
+	{
+		audio_set_rate(atoi(argv[1]));	
+	}
+	if (argc > 2)
+	{
+		audio_set_buffer_len(atoi(argv[2]));	
+	}
+	if (argc > 3)
+	{
+		audio_set_num_buffers(atoi(argv[3]));	
+	}
 	audio_init();
+
 	engine_attach_callback();
 	while(1);
 	audio_shutdown();
